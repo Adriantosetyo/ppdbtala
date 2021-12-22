6f280b240999973d6d9a702372c2e243b3ac2f9c
Adriantosetyo <86601495+Adriantosetyo@users.noreply.github.com>
2021-12-22 14:41:42 +0700

    Add files via upload

---
+++ database/migrations/2019_04_04_103317_create_calon_peserta_didik_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalonPesertaDidikTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calon_peserta_didik', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nisn', 20);
+            $table->string('nilai_nhun', 15);
+            $table->string('skor_jarak', 15);
+            $table->string('skor_total', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calon_peserta_didik');
+    }
+}
